8242531a68170da6b90f83f3ea9f2c535b16f9a9
Miary Andria <themia@gmail.com>
2023-11-06 19:48:55 -0800

    output format

--- recipes/views.py
+++ recipes/views.py
@@ -20,10 +20,12 @@ def index(request):
 
 
 # Use data from form to query recipes through LLM
-def query_recipes(staple, main, meat, request):
+def query_recipes(staple, main, leaves, meat, request):
+    leaves = ""
 
     prompt = (f"You are a chef who is an expert in African cuisine and knowlegable about African recipes.\n" 
         f"List in a bullet point form at list three African recipes that must contain the following ingredients:\n" 
+        f"Add the following leaves to the ingredidents: {leaves}.\n"
         f" {staple} , {main}, mixed only with the following type of meat: {meat}.\n" 
         f"Use exclusively the meat provided and do not include recipes that have other types of meat from the list. " 
         f"Be thorough and make sure that the recipes contain all the necessary ingredients as well as the " 
@@ -71,15 +73,17 @@ def formatResponse(response):
 def ingredDisplay(request):
     selected_main = "" 
     selected_staple = ""
+    selected_leaves = ""
     selected_meat = ""
     
     formdata = request.POST
 
     selected_main = formdata.get('main')
     selected_staple = formdata.get('staple')
+    selected_leaves = formdata.get('leaves')
     selected_meat = formdata.get('meat')
     
-    llm_response = query_recipes(selected_main, selected_staple, selected_meat, request)
+    llm_response = query_recipes(selected_main, selected_staple, selected_leaves, selected_meat, request)
     
     return render(request, 'response.html', {'llm_response': llm_response})
    
